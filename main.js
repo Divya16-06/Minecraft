a0dfcfe417171b7422fc99fe586fe5d9f5d6f8c5
Divya16-06 <84956761+Divya16-06@users.noreply.github.com>
2022-04-21 20:57:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const { fabric } = require("./fabric");
+
+var canvas= new fabric.Canvas("canvas1");
+player_x=10;
+player_y=10;
+
+widthofblock=30;
+heightofblock=30;
+
+var playerpic="";
+var blockpic="";
+
+function uploadpic(){
+    fabric.Image.fromURL()
+}
+
